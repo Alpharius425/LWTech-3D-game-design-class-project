6c5a42a6ff9d5226b662b6bea9a76aa3425ce48f
Mikey <mikeyp895@gmail.com>
2021-05-08 19:07:38 -0700

    Changed the attacks to what they are in the GDD (IE raycasts and collider based) also made our pickups respawn after the player leaves for certain amount of time and goes a certain distance away

--- Assets/Scripts/PickUps_Misc/AmmoPickUp.cs
+++ Assets/Scripts/PickUps_Misc/AmmoPickUp.cs
@@ -5,15 +5,50 @@ using UnityEngine;
 public class AmmoPickUp : MonoBehaviour
 {
 
-    [SerializeField] AmmoType ammoType;
-    [SerializeField] int value;
+    [SerializeField] AmmoType ammoType; // what kind of ammo the pickup gives
+    [SerializeField] int value; // how much ammo the pickup gives
+    [SerializeField] bool active = true; // has the pickup been used recently
+
+    [SerializeField] float respawnTime; // max time between respawns
+    [SerializeField] float curTimeToRespawn; // how long until it respawns next
+    [SerializeField] GameObject player; // reference for the player
+    [SerializeField] float requiredDistanceFromPlayer; // how far away the player needs to be
+    [SerializeField] float distFromPlayer; // the actual distance from the player
+
+    Renderer myRender; // allows us to reference the renderer easily
 
     private void OnTriggerEnter(Collider col)
     {
-        if(col.CompareTag("Player"))
+        if (col.CompareTag("Player") && active == true) // checks if the player runs into the pickup and that it is active
         {
-            col.GetComponent<Player_Stats>().IncreaseAmmo(ammoType, value);
-            gameObject.SetActive(false);
+            col.GetComponent<Player_Stats>().IncreaseAmmo(ammoType, value); // gives the player ammo
+            curTimeToRespawn = respawnTime; // resets the spawn timer
+            active = false; // turns off the pickup
+            myRender.enabled = false; // turns off the renderer so it looks like it disappeared
         }
+    }
+
+    private void Update()
+    {
+        curTimeToRespawn -= Time.deltaTime; // counts down the respawn timer
+
+        distFromPlayer = Vector3.Distance(gameObject.transform.position, player.transform.position); // calculates the distance from the player
+
+        if(curTimeToRespawn <= 0 && active == false && distFromPlayer >= requiredDistanceFromPlayer) // checks if the timer has counted down, if the pickup is inactive and if the player is far enough away
+        {
+            Reactivate();
+        }
+    }
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player"); // saves the player
+        myRender = gameObject.GetComponent<MeshRenderer>(); // saves our renderer
+    }
+
+    void Reactivate() // turns the pickup back on and the renderer is reactivated
+    {
+        active = true;
+        myRender.enabled = true;
     }
 }
