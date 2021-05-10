8cfd13ee30a5da5436deb56cd6f681afd4cf467f
Mikey <mikeyp895@gmail.com>
2021-05-10 16:51:42 -0700

    updated the way the player gets health and added the health pickup

--- Assets/Scripts/PickUps_Misc/HealthPickUp.cs
+++ Assets/Scripts/PickUps_Misc/HealthPickUp.cs
@@ -2,11 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AmmoPickUp : MonoBehaviour
+public class HealthPickUp : MonoBehaviour
 {
-
-    [SerializeField] AmmoType ammoType; // what kind of ammo the pickup gives
-    [SerializeField] int value; // how much ammo the pickup gives
+    [SerializeField] int value; // how much health the pickup gives
     [SerializeField] bool active = true; // has the pickup been used recently
 
     [SerializeField] float respawnTime; // max time between respawns
@@ -23,7 +21,7 @@ public class AmmoPickUp : MonoBehaviour
     {
         if (col.CompareTag("Player") && active == true) // checks if the player runs into the pickup and that it is active
         {
-            col.GetComponent<Player_Stats>().IncreaseAmmo(ammoType, value); // gives the player ammo
+            col.GetComponent<Player_Stats>().HPRegen(value); // gives the player ammo
             curTimeToRespawn = respawnTime; // resets the spawn timer
             active = false; // turns off the pickup
             myRender.enabled = false; // turns off the renderer so it looks like it disappeared
@@ -36,7 +34,7 @@ public class AmmoPickUp : MonoBehaviour
 
         distFromPlayer = Vector3.Distance(gameObject.transform.position, player.transform.position); // calculates the distance from the player
 
-        if(curTimeToRespawn <= 0 && active == false && distFromPlayer >= requiredDistanceFromPlayer && respawns == true) // checks if the timer has counted down, if the pickup is inactive and if the player is far enough away
+        if (curTimeToRespawn <= 0 && active == false && distFromPlayer >= requiredDistanceFromPlayer && respawns == true) // checks if the timer has counted down, if the pickup is inactive and if the player is far enough away
         {
             Reactivate();
         }
