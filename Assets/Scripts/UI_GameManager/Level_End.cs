e1e3f02dd64137d4bc525e2b95a4fc9452db9f88
Mikey <mikeyp895@gmail.com>
2021-06-13 19:16:49 -0700

    merging grants updated effects

---
+++ Assets/Scripts/UI_GameManager/Level_End.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_End : MonoBehaviour
+{
+    /**
+     * A clunky little script used to trigger the end of our demo level
+     */
+    //=========================FIELDS=========================
+    [SerializeField] Animator whiteFadePanel; //animator object attached to white fade panel
+    [SerializeField] int startSceneIndex = 0; //reference to where the start scene is in the build settings
+    //=========================METHODS=========================
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "Player") //when the player enters this area...
+        {
+            whiteFadePanel.SetTrigger("on"); //tell the animator to start fading out.
+            StartCoroutine("FadeToWhite"); //start the coroutine to wait until the animation is finished
+        }
+    }
+
+    private IEnumerator FadeToWhite()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(startSceneIndex);
+    }
+}
